96a00431f955bfc99e7a3154f52c0dc093663999
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-03-11 03:57:55 +0530

    modified:   app/Components/sidebar/Sidebar.tsx 	modified:   app/Providers/Contextprovider.tsx 	new file:   app/incomplete/page.tsx 	modified:   app/layout.tsx 	new file:   app/utils/Icons.js 	new file:   app/utils/connect.ts 	new file:   app/utils/formatDate.js 	new file:   app/utils/menu.js 	new file:   public/Rohomir.jpg 	new file:   public/avatar1.png

--- app/Components/sidebar/Sidebar.tsx
+++ app/Components/sidebar/Sidebar.tsx
@@ -1,18 +1,192 @@
 "use client";
-import React from 'react'
+import React from 'react';
 import { styled } from 'styled-components';
-import {useGlobalstate }from "@/app/context/globalprovider";
-function Sidebar() {
+import { useGlobalstate } from "@/app/context/globalprovider";
+import Image from "next/image";
+import menu from "@/app/utils/menu";
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 
-  const {theme} = useGlobalstate();
+function Sidebar() {
+  const router = useRouter();
+  const path = usePathname();
+  const handleclick = (link: string) => {
+    router.push(link);
+  }
+  const { theme } = useGlobalstate();
 
   return (
-    <Sidebarstyle>Sidebar</Sidebarstyle>
+    <Sidebarstyle theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay">
+          <div className="image">
+            <Image width={70} height={70} src="/avatar1.png" alt="Avatar" />
+          </div>
+        </div>
+        <h1>
+          <span>Qb</span>
+          <span>Bank</span>
+        </h1>
+      </div>
+      <ul className='nav-items'>
+        {menu.map((item) => {
+          return (
+            <li className={`nav-item ${path == item.link ? "active" : ""}`} onClick={() => { handleclick(item.link) }}>
+              {item.icon}
+              <Link href={item.link}>{item.title}</Link>
+            </li>
+          )
+        })}
+      </ul>
+      <button></button>
+    </Sidebarstyle>
   )
 }
 
 const Sidebarstyle = styled.nav`
+  width: ${(props) => props.theme.sidebarWidth};
+  position: relative;
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+
+    .profile-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(10px);
+      z-index: 0;
+      background: ${(props) => props.theme.colorBg3};
+      transition: all 0.55s linear;
+      border-radius: 1rem;
+      border: 2px solid ${(props) => props.theme.borderColor2};
+      opacity: 0.7;
+    }
+
+    h1 {
+      font-size: 1.2rem;
+      display: flex;
+      flex-direction: column;
+
+      line-height: 1.4rem;
+    }
+
+    .image,
+    h1 {
+      position: relative;
+      z-index: 1;
+    }
+
+    .image {
+      flex-shrink: 0;
+      display: inline-block;
+      overflow: hidden;
+      transition: all 0.5s ease;
+      border-radius: 100%;
+      margin-left: 0.5rem;
+      width: 70px;
+      height: 70px;
+
+      img {
+        border-radius: 100%;
+        transition: all 0.5s ease;
+      }
+    }
+
+    > h1 {
+      margin-left: 3rem;
+      font-size: clamp(1.2rem, 4vw, 1.4rem);
+      line-height: 100%;
+    }
+
+    &:hover {
+      .profile-overlay {
+        opacity: 1;
+        border: 2px solid ${(props) => props.theme.borderColor2};
+      }
+
+      img {
+        transform: scale(1.1);
+      }
+    }
+  }
+
+  .nav-item {
+    position: relative;
+    padding: 0.8rem 1rem 0.9rem 2.1rem;
+    margin: 0.3rem 0;
+
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    cursor: pointer;
+    align-items: center;
+
+    &::after {
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 0;
+      width: 0;
+      height: 100%;
+      background-color: ${(props) => props.theme.activeNavLinkHover};
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
+    }
+
+    &::before {
+      position: absolute;
+      content: "";
+      right: 0;
+      top: 0;
+      width: 0%;
+      height: 100%;
+      background-color: ${(props) => props.theme.colorGreenDark};
+
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+    }
+
+    a {
+      font-weight: 500;
+      transition: all 0.3s ease-in-out;
+      z-index: 2;
+      line-height: 0;
+    }
+
+    i {
+      display: flex;
+      align-items: center;
+      color: ${(props) => props.theme.colorIcons};
+    }
+
+    &:hover {
+      &::after {
+        width: 100%;
+      }
+    }
+  }
 `;
 
-export default Sidebar
+export default Sidebar;
--- app/Providers/Contextprovider.tsx
+++ app/Providers/Contextprovider.tsx
@@ -3,11 +3,21 @@ import React from 'react'
 import GlobalStyleProvider from './GlobalStyleProvider';
 import { GlobalProvider } from '../context/globalprovider';
 
-interface  Props {
-    children : React.ReactNode;
+interface Props {
+  children: React.ReactNode;
 }
 
-function Contextprovider({children}:Props) {
+function Contextprovider({ children }: Props) {
+
+  const [isReady, setisReady] = React.useState(false);
+  React.useEffect(() => {
+    setTimeout(() => { setisReady(true); }, 200)
+  }, []);
+  if(!isReady){
+    return null
+  }
+
+
   return (
     <GlobalProvider>{children}</GlobalProvider>
   )
